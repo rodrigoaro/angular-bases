dc68eb63109c411737a76ca19108f845167b68d0
Rodrigo Aro Ringele <rarorin@contraloria.cl>
2024-11-12 12:01:33 -0300

    Fin de sección 4 del curso.

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,28 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-counter',
+    template: `
+        <h3>Counter: {{ counter }}</h3>
+        <button (click)="increaseBy()">+1</button>
+        <button (click)="reset()">Reset</button>
+        <button (click)="substract()">-1</button>
+    `
+})
+
+export class CounterComponent {
+
+    public counter: number = 10;
+
+    increaseBy(): void{
+        this.counter += 1;
+    }
+
+    substract(): void{
+        this.counter -= 1;
+    }
+
+    reset(): void {
+        this.counter = 10;
+    }
+}
